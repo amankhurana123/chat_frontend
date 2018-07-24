c4c4d3fc7766f315a1574b6fea3becd91af10934
prince <prince.verma@daffodilsw.com>
2018-07-24 17:25:10 +0530

    chat screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import { Route, Switch ,withRouter} from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
+import openSocket from "socket.io-client";
+import config from "./config";
 import Login from "./container/login";
 import Home from "./container/home";
 
 class App extends Component {
   render() {
+    const socket = openSocket(config.serverUrl);
+
     return (
       <Switch>
         <Route exact path="/" component={Login} />
--- src/container/home.jsx
+++ src/container/home.jsx
@@ -1,13 +1,116 @@
 import React, { Component } from "react";
+import { capitalize } from "lodash";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import Send from "@material-ui/icons/Send";
+import { apiInstance } from "../api";
 import "../styles.css";
 
 export default class Home extends Component {
-  componentDidMount() {
-    console.log("====================================");
-    console.log();
-    console.log("====================================");
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
+      name: "",
+      email: "",
+      id: "",
+      startChating: false
+    };
   }
+
+  componentWillMount = async () => {
+    const user = localStorage.getItem("user");
+    const params = encodeURI(JSON.stringify(JSON.parse(user).data._id));
+
+    const options = {
+      method: "get",
+      url: "/user/getUser?params=" + params
+    };
+    apiInstance(options)
+      .then(response => {
+        this.setState({ user: response.data });
+        console.log("====================================");
+        console.log("response", this.state.user);
+
+        console.log("====================================");
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  };
+  onHandleChangeState = (name, email, id, startChating) => {
+    this.setState({ name, email, id, startChating });
+  };
+
   render() {
-    return <div className="mainContainer">Hello world</div>;
+    return (
+      <div className="mainHomeContainer">
+        <div className="header" />
+        <div className="body">
+          <div className="leftContainer">
+            {this.state.user.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="list"
+                  onClick={() => {
+                    this.onHandleChangeState(
+                      item.name,
+                      item.email,
+                      item._id,
+                      true
+                    );
+                  }}
+                >
+                  <img
+                    src={require("./../image/images.png")}
+                    width="40px"
+                    height="40px"
+                    alt="user"
+                  />
+                  <div className="titleAndSubtitle">
+                    <div className="title">{capitalize(item.name)}</div>
+                    <div className="subtitle">{item.email}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {this.state.startChating ? (
+            <div className="rightContainer">
+              <div className="rightHeader">
+                <div className="leftRightHeaderComponent">
+                  <ArrowBack
+                    onClick={() => {
+                      this.onHandleChangeState("", "", "", false);
+                    }}
+                    className="arrowName"
+                  />
+                  <div className="chatUsername">
+                    <div>{capitalize(this.state.name)}</div>
+                    <div>{this.state.email}</div>
+                  </div>
+                </div>
+                <div />
+              </div>
+              <div className="rightBody">centerr</div>
+              <div className="rightFooter">
+                <div className="innerRightFooter">
+                  <textarea className="textarea" />
+                  <div className="sendButton">
+                    <Send className="send" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="rightContainer">
+              <div className="imageRightContainser">
+                <img src={require("./../image/chat1.png")} alt="user" />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
