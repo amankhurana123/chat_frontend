3d3945662b11bf99b095f4a88878d97fb749d5a5
amankhurana123 <aman.khurana@daffodilsw.com>
2018-07-25 08:32:47 +0530

    chat message

--- src/container/home.jsx
+++ src/container/home.jsx
@@ -12,8 +12,10 @@ export default class Home extends Component {
       user: [],
       name: "",
       email: "",
-      id: "",
-      startChating: false
+      toUser: "",
+      startChating: false,
+      chatText: "",
+      startChatTextField: false
     };
   }
 
@@ -37,10 +39,44 @@ export default class Home extends Component {
         console.log("error", error);
       });
   };
-  onHandleChangeState = (name, email, id, startChating) => {
-    this.setState({ name, email, id, startChating });
+  onHandleChangeState = (name, email, toUser, startChating) => {
+    this.setState({ name, email, toUser, startChating });
+    const options ={
+      method: "get",
+      url: '/chat/'
+    }
+    apiInstance(options)
+  .then(response=>{
+    console.log("responser>>>>>>>>>>>>>>>>>>>>>",response)
+    this.setState({startChatTextField : false, chatText: ""})
+    }).catch(error=>{
+    console.log("error",error)
+    })
+}
+  onChangeState=(event)=>{
+    this.setState({chatText: event.target.value})
+  }
+  chatMessages=()=>{
+    this.setState({startChatTextField : true})
+  const {chatText, toUser, }=this.state;
+  const fromUser= encodeURI(JSON.stringify(JSON.parse(localStorage("user")).data._id));
+  const headers = {
+    "content-type": "application/json",
+    Accept: "application/json"
   };
-
+  const options = {
+    method: "post",
+    url: "/chat/",
+    data: { chatText ,toUser,fromUser },
+    headers
+  };
+  apiInstance(options)
+  .then(response=>{
+    console.log("responser>>>>>>>>>>>>>>>>>>>>>",response)
+}).catch(error=>{
+  console.log("error",error)
+})
+}
   render() {
     return (
       <div className="mainHomeContainer">
@@ -56,7 +92,7 @@ export default class Home extends Component {
                     this.onHandleChangeState(
                       item.name,
                       item.email,
-                      item._id,
+                      item.toUser,
                       true
                     );
                   }}
@@ -92,11 +128,11 @@ export default class Home extends Component {
                 </div>
                 <div />
               </div>
-              <div className="rightBody">centerr</div>
+              <div className="rightBody">{this.state.startChatTextField && <div className="chat"><p>{this.state.chatText}</p></div>}</div>
               <div className="rightFooter">
                 <div className="innerRightFooter">
-                  <textarea className="textarea" />
-                  <div className="sendButton">
+                  <textarea className="textarea" value={this.state.chatText} onChange={this.onChangeState}  />
+                  <div className="sendButton" onClick={this.chatMessages} >
                     <Send className="send" />
                   </div>
                 </div>
